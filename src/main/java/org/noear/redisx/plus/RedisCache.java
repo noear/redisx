574b65e8bb60a144631ac4ed90d625e6701c5d77
noear <noear@live.cn>
2021-10-14 11:17:34 +0800

    1.2

--- src/main/java/org/noear/redisx/plus/RedisCache.java
+++ src/main/java/org/noear/redisx/plus/RedisCache.java
@@ -7,6 +7,8 @@ import org.noear.redisx.utils.SerializationUtil;
 import java.util.Base64;
 
 /**
+ * Redis cache
+ *
  * @author noear
  * @since 1.0
  */
@@ -18,10 +20,16 @@ public class RedisCache {
     }
 
 
+    /**
+     * 存储
+     * */
     public void store(String key, String val, int inSeconds) {
         client.open(session -> session.key(key).expire(inSeconds).set(val));
     }
 
+    /**
+     * 存储并序列化
+     * */
     public void storeAndSerialize(String key, Object obj, int inSeconds) {
         AssertUtil.notNull(obj,"redis value cannot be null");
 
@@ -31,10 +39,16 @@ public class RedisCache {
         client.open(session -> session.key(key).expire(inSeconds).set(val));
     }
 
+    /**
+     * 获取
+     * */
     public String get(String key) {
         return client.openAndGet(session -> session.key(key).get());
     }
 
+    /**
+     * 获取并反序列化
+     * */
     public <T> T getAndDeserialize(String key) {
         String val = client.openAndGet(session -> session.key(key).get());
 
@@ -46,6 +60,9 @@ public class RedisCache {
         }
     }
 
+    /**
+     * 移除
+     * */
     public void remove(String key) {
         client.open(session -> session.key(key).delete());
     }

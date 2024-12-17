2f00fb3f75b84730704cea2acff7fac67f601a6e
linziguan <linziguan@live.com>
2024-12-17 13:48:09 +0800

    增加psubscribe,用于redis事件监听

--- src/main/java/org/noear/redisx/plus/RedisBus.java
+++ src/main/java/org/noear/redisx/plus/RedisBus.java
@@ -48,6 +48,37 @@ public class RedisBus {
 
         return future;
     }
+    
+    /**
+     * 根据匹配模式patterns进行订阅
+     * 常用于redis事件监听
+     */
+    public void psubscribe(BiConsumer<String, String> consumer, String... patterns) {
+		client.open(s -> {
+            s.jedis().psubscribe(new JedisPubSub() {
+                @Override
+                public void onPMessage(String pattern,String channel, String message) {
+                    consumer.accept(channel, message);
+                }
+            }, patterns);
+        });
+	}
+	
+	public CompletableFuture<Thread> psubscribeFuture(BiConsumer<String, String> consumer, String... patterns) {
+		CompletableFuture<Thread> future = new CompletableFuture();
+
+        Thread thread = new Thread(() -> {
+            try {
+            	psubscribe(consumer,patterns);
+                future.complete(Thread.currentThread());
+            } catch (Throwable e) {
+                future.completeExceptionally(e);
+            }
+        });
+        thread.start();
+
+        return future;
+	}
 
     /**
      * 发布
